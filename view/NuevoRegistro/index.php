3edf421e2ee325a183c39bb29f7b9fc9899a221c
JhonatanAndree <43083376+JhonatanAndree@users.noreply.github.com>
2021-07-13 02:35:28 -0500

    Frontend 01

---
+++ view/NuevoRegistro/index.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="es" class="no-focus">
+
+<head>
+    <?php require_once('../Mainhead/mainhead.php'); ?>
+    <title>Nuevo Registro | Mesa de partes</title>
+</head>
+
+<body>
+    <div id="page-container" class="sidebar-o side-scroll page-header-modern main-content-boxed">
+        <aside id="side-overlay">
+            <div id="side-overlay-scroll">
+                <div class="content-header content-header-fullrow">
+                    <div class="content-header-section align-parent">
+                        <button type="button" class="btn btn-circle btn-dual-secondary align-v-r" data-toggle="layout" data-action="side_overlay_close">
+                            <i class="fa fa-times text-danger"></i>
+                        </button>
+                        <div class="content-header-item">
+                            <a class="img-link mr-5" href="be_pages_generic_profile.html">
+                                <img class="img-avatar img-avatar32" src="../../public/assets/img/avatars/avatar15.jpg" alt="">
+                            </a>
+                            <a class="align-middle link-effect text-primary-dark font-w600" href="be_pages_generic_profile.html">John Smith</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </aside>
+        <nav id="sidebar">
+            <div id="sidebar-scroll">
+                <div class="sidebar-content">
+                    <?php require_once('../MainSidebar/mainsidebar.php'); ?>
+                    <?php require_once('../MainMenu/mainmenu.php'); ?>
+                </div>
+            </div>
+        </nav>
+        <?php require_once("../Mainheader/mainheader.php"); ?>
+
+        <!-- Contenido -->
+        <main id="main-container">
+            <div class="content">
+
+                <div class="block">
+                    <div class="block-header block-header-default">
+                        <h3 class="block-title">Nuevo Registro <small>Mesa de Partes</small></h3>
+                    </div>
+
+                    <div class="block-content block-content-full">
+
+                        <div class="form-group row">
+                            <label class="col-12" for="example-text-input">Asunto</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" id="example-text-input" name="example-text-input">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-12" for="example-textarea-input">Descripción</label>
+                            <div class="col-12">
+                                <textarea class="form-control" id="example-textarea-input" name="example-textarea-input" rows="6"></textarea>
+                            </div>
+                        </div>
+
+                        <!-- Botones -->
+                        <div class="block-content block-content-sm block-content-full bg-body-light">
+                            <div class="row">
+                                <div class="col-6">
+                                    <button type="button" class="btn btn-alt-info">
+                                        <i class="fa fa-file-pdf-o"></i> Adjuntar Documentos
+                                    </button>
+                                </div>
+                                <div class="col-6 text-right">
+                                    <button type="button" class="btn btn-alt-primary">
+                                        Guardar <i class="fa fa-save ml-5"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Botones -->
+
+                    </div>
+                </div>
+
+                <div class="block">
+                    <div class="block-header block-header-default">
+                        <h3 class="block-title">Listado de Documentos <small>Mesa de Partes</small></h3>
+                    </div>
+
+                    <div class="block-content block-content-full">
+                        <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
+                            <thead>
+                                <tr>
+                                    <th class="text-center"></th>
+                                    <th>Name</th>
+                                    <th class="d-none d-sm-table-cell">Email</th>
+                                    <th class="d-none d-sm-table-cell" style="width: 15%;">Access</th>
+                                    <th class="text-center" style="width: 15%;">Profile</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-center">1</td>
+                                    <td class="font-w600">Barbara Scott</td>
+                                    <td class="d-none d-sm-table-cell">customer1@example.com</td>
+                                    <td class="d-none d-sm-table-cell">
+                                        <span class="badge badge-danger">Disabled</span>
+                                    </td>
+                                    <td class="text-center">
+                                        <button type="button" class="btn btn-sm btn-secondary" data-toggle="tooltip" title="View Customer">
+                                            <i class="fa fa-user"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center">2</td>
+                                    <td class="font-w600">Laura Carr</td>
+                                    <td class="d-none d-sm-table-cell">customer2@example.com</td>
+                                    <td class="d-none d-sm-table-cell">
+                                        <span class="badge badge-success">VIP</span>
+                                    </td>
+                                    <td class="text-center">
+                                        <button type="button" class="btn btn-sm btn-secondary" data-toggle="tooltip" title="View Customer">
+                                            <i class="fa fa-user"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+
+        </main>
+        <!-- Contenido -->
+
+        <?php require_once("../MainFooter/mainfooter.php"); ?>
+    </div>
+    <?php require_once("../MainJs/mainjs.php"); ?>
+</body>
+
+</html>
