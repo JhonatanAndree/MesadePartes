6694b79c2200faf082e2f2986a9e1c2f6c726058
JhonatanAndree <43083376+JhonatanAndree@users.noreply.github.com>
2021-07-13 18:19:21 -0500

    Login Acceso listo

---
+++ config/conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+class Conectar {
+    protected $dbh;
+    protected function Conexion(){
+        try {
+            $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=mesadepartes","root","");
+            return $conectar;
+        } catch (Exception $e) {
+            print "¡Error en la base de datos !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function set_names(){
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+
+    public function ruta(){
+        return "http://localhost/mesadepartes/";
+    }
+}
+
+?>
